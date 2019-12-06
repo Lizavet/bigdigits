0d029ab2db31ffd1a4bca2601265da8d1cc95995
Lizaveta <liza2000kotika@yandex.ru>
2019-12-06 22:24:58 +0300

    улучшенный вариант вывода цифр друг за другом с новой строки

--- bigdigits5.py
+++ bigdigits5.py
@@ -35,11 +35,12 @@ Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 import sys
 p=sys.argv[1]
 
-second=(0,1,2,3,4,5,6)
-
 for i in p:
 	a=int(i)
-	for x in second:
+	x=0
+	while x <= 6:
 		print(Digits[a][x])
+		x+=1
+		
 
 
